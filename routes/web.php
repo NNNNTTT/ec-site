1c3575dd6e93004ab4e331b9780423f58d8749b4
NNNNTTT <arsenal45@icloud.com>
2025-08-03 10:39:57 +0900

    breezeを追加

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Middleware\CartSession;
-use App\Http\Controllers\LineItemController;
-use App\Http\Controllers\CartController;
 
-/*Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
-});*/
-
-Route::middleware([CartSession::class])->group(function () {
-
-    Route::name('product')->group(function () {
-        Route::get('/', [ProductController::class, 'index'])->name('.index');
-        Route::get('/product/{id}', [ProductController::class, 'show'])->name('.show');
-    });
-
-    Route::name('line_item')->group(function(){
-        Route::post('/line_item/create', [LineItemController::class, 'create'])->name('.create');
-        Route::post('/line_item/delete', [LineItemController::class, 'delete'])->name('.delete');
-    });
-
-    Route::name('cart')->group(function(){
-        Route::get('/cart', [CartController::class, 'index'])->name('.index');
-        Route::get('/cart/checkout', [CartController::class, 'checkout'])->name('.checkout');
-        Route::get('/cart/success', [CartController::class, 'success'])->name('.success');
-    });
-
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
