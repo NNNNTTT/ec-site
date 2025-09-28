bdf9729ab2af02055b7bb740a2b199dc0f1fe4d9
NNNNTTT <arsenal45@icloud.com>
2025-09-28 12:54:03 +0900

    注文ステータス更新機能の改善: AdminOrderControllerにおいて、決済確定およびキャンセル処理の条件を整理し、支払い方法に応じた処理を明確化。未払いの注文に対するステータス更新のロジックを強化し、コードの可読性を向上させました。

--- app/Http/Controllers/AdminOrderController.php
+++ app/Http/Controllers/AdminOrderController.php
@@ -33,24 +33,41 @@ class AdminOrderController extends Controller
             if($order['status'] != 'no_change'){
                 $old_order = Order::find($id);
                 if($old_order->status != $order['status']){
+
+                    if($old_order->payment_method == 'credit_card'){
+                        //決済確定処理
+                        if($order['status'] == 'shipped' && $old_order->payment_status == 'unpaid'){
+                            $stripeService = new StripeService();
+                            $stripeService->capture($old_order);
+                            $old_order->stripe_capture = now();
+                            $old_order->payment_status = 'paid';
+                        }
+
+                        //決済キャンセル処理
+                        else if($order['status'] == 'canceled' && $old_order->payment_status == 'unpaid'){
+                            $stripeService = new StripeService();
+                            $stripeService->cancel($old_order);
+                            $old_order->stripe_cancel = now();
+                            $old_order->payment_status = 'canceled';
+                        }
+
+                    }else{
+
+                        if($order['status'] == 'shipped'){
+                            $old_order->payment_status = 'paid';
+                        }
+
+                        else if($order['status'] == 'canceled'){
+                            $old_order->payment_status = 'canceled';
+                        }
+
+                        else if($order['status'] == 'pending'){
+                            $old_order->payment_status = 'unpaid';
+                        }
+
+                    }
+
                     $old_order->status = $order['status'];
-
-                    //決済確定処理
-                    if($order['status'] == 'shipped' && $old_order->payment_method == 'credit_card' && $old_order->payment_status == 'unpaid'){
-                        $stripeService = new StripeService();
-                        $stripeService->capture($old_order);
-                        $old_order->stripe_capture = now();
-                        $old_order->payment_status = 'paid';
-                    }
-
-                    //決済キャンセル処理
-                    if($order['status'] == 'canceled' && $old_order->payment_method == 'credit_card' && $old_order->payment_status == 'unpaid'){
-                        $stripeService = new StripeService();
-                        $stripeService->cancel($old_order);
-                        $old_order->stripe_cancel = now();
-                        $old_order->payment_status = 'canceled';
-                    }
-
                     $old_order->save();
                 }
             }
