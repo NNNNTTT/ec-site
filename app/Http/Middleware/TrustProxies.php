1b48b5044d607592465cfcfb1e2590d837c2c297
NNNNTTT <arsenal45@icloud.com>
2025-10-08 01:51:12 +0900

    20251007 TrustProxies.phpをSymfonyを使う形へ変更

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,19 +3,19 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
-use Illuminate\Http\Request;
+// ここを追加
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 
 class TrustProxies extends Middleware
 {
-    /**
-     * Traefik 経由なら '*' でもOK。
-     * もっと厳密にするなら Docker ネットワークのCIDRに置き換え:
-     * 例) protected $proxies = ['172.18.0.0/16'];
-     */
     protected $proxies = '*';
 
-    /**
-     * X-Forwarded-* ヘッダ群を使用
-     */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    // Symfony の定数を使う
+    protected $headers =
+        SymfonyRequest::HEADER_X_FORWARDED_FOR
+        | SymfonyRequest::HEADER_X_FORWARDED_HOST
+        | SymfonyRequest::HEADER_X_FORWARDED_PROTO
+        | SymfonyRequest::HEADER_X_FORWARDED_PORT;
+    // もしくは環境が対応していれば
+    // protected $headers = SymfonyRequest::HEADER_X_FORWARDED_ALL;
 }
