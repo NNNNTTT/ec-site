90c785bec45ce25938f89c8363539f28b36fe881
NNNNTTT <arsenal45@icloud.com>
2025-11-28 22:52:07 +0900

    20251128 カート画面のUIを変更 PC版のみ

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -4,52 +4,64 @@
 カート
 @endsection
 
+@section('css', asset('css/cart/index.css'))
+
 @section('content')
 <div class="container">
     <div class="cart__title">
-        Shopping Cart
+        ショッピングカート
     </div>
     @if(count($line_items) > 0)
-    <div class="cart-wrapper">
-        @foreach ($line_items as $item)
-        <div class="card mb-3 card_hover">
-            <div class="row">
-                <img src="{{ asset($item->image) }}" alt="{{ $item->name }}" class="product-cart-img col-2"/>
-                <div class="card-body col-9">
-                    <div class="card-product-name col-6">
-                        {{ $item->name }}
-                    </div>
-                    <div class="card-quantity col-2">
-                        {{ $item->pivot->quantity }}個
-                    </div>
-                    <div class="card__total-price col-3 text-center">
-                        ￥{{ number_format($item->price * $item->pivot->quantity) }}
-                    </div>
+    <p class='mb-5'>ご利用ありがとうございます<br>ショッピングカートには下記の商品が入っています。</p>
+    <table class='table'>
+        <thead>
+            <tr class='top-tr align-middle'>
+                <th scope='col' class='w-50'>商品名</th>
+                <th scope='col'>価格</th>
+                <th scope='col'>数量</th>
+                <th scope='col'>小計</th>
+                <th scope='col'>削除</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($line_items as $item)
+            <tr class='align-middle'>
+                <td scope='row'>{{ $item->name }}</td>
+                <td>¥{{ number_format($item->price) }}</td>
+                <td>{{ $item->pivot->quantity }}</td>
+                <td>¥{{ number_format($item->price * $item->pivot->quantity) }}</td>
+                <td>
                     <form action="{{ route('line_item.delete') }}" method='POST'>
                         @csrf
-                        <div class='card__btn-trash col-1'>
-                            <input type='hidden' name='id' value='{{ $item->pivot->id}}'>
-                                <button type="submit" class="btn btn-sm delete-btn">
-                                    <i class="fas fa-trash-alt"></i>
-                                </button>
-                        </div>
+                        <input type='hidden' name='id' value='{{ $item->pivot->id}}'>
+                        <button type='submit' class='btn btn-outline-secondary'>削除</button>
                     </form>
-                </div>
-            </div>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+        <tfoot>
+            <tr class='align-middle'>
+                <td colspan="4" class='text-end'>商品合計（税込）</td>
+                <td>¥{{ number_format($total_price) }}</td>
+            </tr>
+        </tfoot>
+    </table>
+
+    <div class="cart_btn">
+        <div class='back_btn'>
+            <a href="{{ route('product.index') }}" class="">お買い物を続ける</a>
         </div>
-        @endforeach
+        <div class='purchase_btn'>
+            <a href="{{ route('order.index') }}" class="">購入手続きに進む</a>
+        </div>
+        
     </div>
-    <div class="cart__sub-total">
-        小計：￥{{ number_format($total_price) }}
-    </div>
-    <button onClick="location.href='{{ route('order.index') }}'" class='cart__purchase btn btn-dark'>
-        購入手続きに進む
-    </button>
-    <a href="{{ route('product.index') }}" class="cart__purchase btn btn-outline-secondary mx-3">お買い物を続ける</a>
     @else
     <div class="cart__empty">
         カートに商品が入っていません。
     </div>
     @endif
+
 </div>
 @endsection
