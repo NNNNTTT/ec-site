6a35bbde77375cbf5e76ddedf9bea91011a73719
NNNNTTT <arsenal45@icloud.com>
2025-12-07 19:44:23 +0900

    251207 注文完了ページのUIを変更

--- resources/views/order/success.blade.php
+++ resources/views/order/success.blade.php
@@ -4,6 +4,8 @@
     注文完了
 @endsection
 
+@section('css', asset('css/order/success.css'))
+
 @section('content')
     <div class="container my-5">
         <div class="row">
@@ -11,7 +13,7 @@
                 <h1 class="text-center mb-4">注文完了</h1>
                 <p class="text-center mb-4">注文が完了しました。引き続きお買い物をお楽しみください。</p>
                 <div class="text-center">
-                    <a href="{{ route('product.index') }}" class="btn btn-dark text-white">商品一覧へ戻る</a>
+                    <a href="{{ route('product.index') }}" class="product-btn">商品一覧へ戻る</a>
                 </div>
             </div>
         </div>
