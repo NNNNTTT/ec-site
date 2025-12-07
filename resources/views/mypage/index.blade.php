9048943adcb3e3c819eaaa315281de747c5b444d
NNNNTTT <arsenal45@icloud.com>
2025-12-07 19:28:45 +0900

    251207 マイページ画面のUIを変更。

--- resources/views/mypage/index.blade.php
+++ resources/views/mypage/index.blade.php
@@ -2,22 +2,9 @@
 
 @section('title', 'マイページ')
 
+@section('css', asset('css/mypage/index.css'))
+
 @section('content')
-<style>
-    .nav-item a:hover {
-        background-color: #f0f0f0;
-        color: inherit;
-    }
-    .account-table td {
-        padding: 20px;
-    }
-    .content {
-        display: none;
-    }
-    .content.active {
-        display: block;
-    }
-</style>
 
 <div class='container mt-5'>
     <h2>マイページ</h2>
@@ -26,7 +13,7 @@
             {{ session('success') }}
         </div>
     @endif
-    <ul class='nav nav-tabs mt-5 mb-5'>
+    <ul class='nav nav-tabs'>
         <li class='nav-item'>
             <a href='#account' class='nav-link active'>アカウント情報</a>
         </li>
@@ -38,35 +25,35 @@
     <div id="account" class="content">
         <table class="table account-table">
             <tr> 
-                <td class="col-3">ユーザー名</td>
-                <td class="col-9">{{ Auth::user()->name }}</td>
+                <td class="col-6 col-sm-3">ユーザー名</td>
+                <td class="col-6 col-sm-9">{{ Auth::user()->name }}</td>
             </tr>
             <tr>    
-                <td class="col-3">メールアドレス</td>
-                <td class="col-9">{{ Auth::user()->email }}</td>
+                <td>メールアドレス</td>
+                <td>{{ Auth::user()->email }}</td>
             </tr>
             <tr>
-                <td class="col-3">電話番号</td>
-                <td class="col-9">{{ Auth::user()->phone }}</td>
+                <td>電話番号</td>
+                <td>{{ Auth::user()->phone }}</td>
             </tr>
             <tr>
-                <td class="col-3">郵便番号</td>
-                <td class="col-9">{{ Auth::user()->postal_code }}</td>
+                <td>郵便番号</td>
+                <td>{{ Auth::user()->postal_code }}</td>
             </tr>
             <tr>
-                <td class="col-3">ご住所（都道府県）</td>
-                <td class="col-9">{{ Auth::user()->prefecture }}</td>
+                <td>ご住所（都道府県）</td>
+                <td>{{ Auth::user()->prefecture }}</td>
             </tr>
             <tr>
-                <td class="col-3">ご住所（市区町村-番地）</td>
-                <td class="col-9">{{ Auth::user()->address }}</td>
+                <td>ご住所（市区町村-番地）</td>
+                <td>{{ Auth::user()->address }}</td>
             </tr>
         </table>
-        <div class="d-flex mt-5 gap-3">
+        <div class="mt-5">
             <a href="{{ route('mypage.edit') }}" class="btn btn-outline-secondary">アカウント情報を編集する</a>
             <form method="POST" action="{{ route('logout') }}">
                 @csrf
-                <button type="submit" class="btn btn-outline-secondary">ログアウト</button>
+                <button type="submit" class="mt-3 btn btn-outline-secondary">ログアウト</button>
             </form>
         </div>
     </div>
@@ -75,25 +62,17 @@
         <table class="table order-history-table">
             <thead>
                 <tr>
-                    <th class="col-1">注文番号</th>
-                    <th class="col-3">注文日</th>
-                    <th class="col-3">注文商品</th>
-                    <th class="col-2">注文金額</th>
-                    <th class="col-3">注文詳細</th>
+                    <th class="col-2">注文番号</th>
+                    <th class="col-5">注文日</th>
+                    <th class="col-5">注文詳細</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($orders as $order)
                 <tr>
-                    <td class="col-1">{{ $order->id }}</td>
-                    <td class="col-3">{{ $order->created_at->format('Y-m-d') }}</td>
-                    <td class="col-3">
-                        @foreach ($order->products as $product)
-                            {{ $product->name }}
-                        @endforeach
-                    </td>
-                    <td class="col-2">{{ $order->total_price }}</td>
-                    <td class="col-3"><a href="{{ route('mypage.order_detail', $order->id) }}" class="btn btn-outline-secondary">注文詳細</a></td>
+                    <td class="col-2">{{ $order->id }}</td>
+                    <td class="col-5">{{ $order->created_at->format('Y-m-d') }}</td>
+                    <td class="col-5"><a href="{{ route('mypage.order_detail', $order->id) }}" class="detail-btn">注文詳細</a></td>
                 </tr>
                 @endforeach    
             <tbody>
